cb5a15eb5b9c3bf01b6d226eb0b8c8e42de0cfe5
raymond <raymond@justering.com>
2016-08-03 22:16:08 +0800

    MOD 增加生成校验参数和验证校验参数的方法

--- src/Authenticator.php
+++ src/Authenticator.php
@@ -11,26 +11,21 @@ use Lingxi\Signature\Interfaces\AuthenticatorInterface;
 
 class Authenticator implements AuthenticatorInterface
 {
-    protected $version;
+    protected $api_key;
+    protected $api_secret;
 
-    public function __construct($version = 'v1')
+    public function __construct($api_key = "", $api_secret = "")
     {
-        $this->version = $version;
+        $this->api_key = $api_key;
+        $this->api_secret = $api_secret;
+
     }
 
     public function attempt($params)
     {
-        return $this->chechVersion($params['version'], $this->version)
-             ->checkTimestamp($params['stamp'])
-             ->checkSignatureKey($params['app_key'])
+        return $this->checkTimestamp($params['stamp'])
+             ->checkSignatureKey($params['api_key'])
              ->checkSignatureValue($params);
-    }
-
-    protected function chechVersion($requestVersion, $version)
-    {
-        if (VersionChecker::check($requestVersion, $version)) {
-            return $this;
-        }
     }
 
     protected function checkTimestamp($timstamp)
@@ -65,9 +60,10 @@ class Authenticator implements AuthenticatorInterface
 
     protected function handleAllSignatureParamaters($params)
     {
-        $params = collect($params)->except(['signature', 'version'])->toArray();
+        $params = collect($params)->except(['signature', 'api_key'])->toArray();
 
-        natsort($params);
+        ksort($params, SORT_STRING);
+        reset($params);
 
         return $params;
     }
@@ -88,4 +84,52 @@ class Authenticator implements AuthenticatorInterface
 
         return $this;
     }
+
+    public function getAuthParams($params){
+        $noncestr = Helpers::createNonceStr();
+        $params['stamp'] = time();
+        $params['noncestr'] = Helpers::createNonceStr();
+        $params['api_key'] = $this->api_key;
+        $sign = $this->genSign($params);
+        $params['signature'] = $sign;
+        return $params;
+    }
+
+    public function verify($params){
+        $need_to_check_sign = $params['signature'];
+        unset($params["signature"]);
+        $sign = $this->genSign($params);
+        return ($sign == $need_to_check_sign);
+    }
+
+    /**
+     * Sign签名生成方法
+     *
+     * @param array $para
+     * @throws Exception
+     * @return string
+     */
+    protected function genSign(array $para)
+    {
+        /**
+         * a.除sign 字段外，对所有传入参数按照字段名的ASCII 码从小到大排序（字典序）后，
+         * 使用URL 键值对的格式（即key1=value1&key2=value2…）拼接成字符串string1，
+         * 注意： 值为空的参数不参与签名 ；
+         */
+        // 过滤不参与签名的参数
+        $paraFilter = Helpers::paraFilter($para);
+        // 对数组进行（字典序）排序
+        $paraFilter = Helpers::argSort($paraFilter);
+        // 进行URL键值对的格式拼接成字符串string1
+        $str = Helpers::createLinkstring($paraFilter);
+        /**
+         * b. 在string1 最后拼接上key=Key(商户支付密钥 ) 得到stringSignTemp 字符串，
+         * 并对stringSignTemp 进行md5 运算，再将得到的字符串所有字符转换为大写，得到sign 值signValue。
+         */
+        return hash_hmac('sha256', $str, $this->getSignatureApiSecret());
+        // $sign = strtoupper(md5($string1));
+        // return $sign;
+    }
+
+
 }
