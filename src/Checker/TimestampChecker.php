b1bf1a58e636889cc97282023743183900312a27
rrylee <qq215672398@126.com>
2016-08-23 11:15:42 +0800

    MOD 修改时间 600

--- src/Checker/TimestampChecker.php
+++ src/Checker/TimestampChecker.php
@@ -7,7 +7,7 @@ use Lingxi\Signature\Exceptions\SignatureTimestampException;
 
 class TimestampChecker implements CheckerInterface
 {
-    const TIME_EXPIRED = 3600;
+    const TIME_EXPIRED = 600;
 
     public static function check($timestamp, $now = null)
     {
