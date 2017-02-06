3d3bf12ceaeb069d7e70c0fdb206b88b47322ed0
lirui <lirui@justering.com>
2017-02-06 15:05:19 +0800

    MOD 去掉参与签名数据中的 null

--- src/Helpers.php
+++ src/Helpers.php
@@ -36,7 +36,9 @@ class Helpers
     {
         $para_filter = array();
         while (list ($key, $val) = each($para)) {
-            if (strtolower(trim($key)) === "signature" || (!is_array($val) && trim($val) === "") || (is_array($val) && empty($val)))
+            if (strtolower(trim($key)) === "signature"
+                || (!is_array($val) && (trim($val) === "" || $val === null))
+                || (is_array($val) && empty($val)))
                 continue;
             else
                 $para_filter[$key] = $para[$key];
